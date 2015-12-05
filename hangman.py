dcb155ee427c13fec8b77986546cda1e51e8d6f9
Ryan Walter <ryman16@vt.edu>
2015-12-05 11:28:16 -0500

    Implemented pseudo random word generation

--- hangman.py
+++ hangman.py
@@ -1,3 +1,9 @@
 #! /usr/bin/python
 
-print "Hello World"
+print "Welcome to HANGMAN"
+word_list = ['linux', 'fedora', 'engineering', 'UNIX', 'Thompson', 'Ritchie', 'programming', 'bash', 'script', 'shell'];
+import random;
+word = word_list[random.randrange(0,10,1)];
+print word;
+
+
