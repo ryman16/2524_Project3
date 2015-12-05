a1a47254af9c31492b82dc3c413b8614b32962a0
Ryan Walter <ryman16@vt.edu>
2015-12-05 13:06:32 -0500

    loops and allows user input

--- hangman.py
+++ hangman.py
@@ -1,9 +1,25 @@
 #! /usr/bin/python
 
 print "Welcome to HANGMAN"
-word_list = ['linux', 'fedora', 'engineering', 'UNIX', 'Thompson', 'Ritchie', 'programming', 'bash', 'script', 'shell'];
+word_list = ['linux', 'fedora', 'engineering', 'unix', 'Thompson', 'Ritchie', 'programming', 'bash', 'script', 'shell'];
 import random;
 word = word_list[random.randrange(0,10,1)];
-print word;
+correct = ['*' for i in word];
+txt = '';
+
+for j in range(len(word)):
+	txt += str(correct[j]);
+
+for num in range(1,11):
+	print "Try #", num;
+	print "Secret word: ", txt;	
+	guess = raw_input("Guess: ");
+	for k in range(len(word)):
+		if guess == word[k]:
+			correct[k] = guess;
+	txt = '';
+	for j in range(len(word)):
+		txt += str(correct[j]);	
+
 
 
